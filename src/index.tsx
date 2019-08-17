aff5004ac30a7bbed39ef2f25d9e89dc84e72279
Samuel Parsons <samuel.m.parsons@gmail.com>
2019-08-17 15:32:44 -0400

    Adding material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { MainPage } from "./videoPlayer"
+import { MainPage } from './videoPlayer'
 
 ReactDOM.render(<MainPage />, document.getElementById('root'))
--- src/videoPlayer/mainPage.tsx
+++ src/videoPlayer/mainPage.tsx
@@ -1,5 +1,14 @@
+import Button from '@material-ui/core/Button'
 import * as React from 'react'
 
 export const MainPage = (): JSX.Element => (
-    <div>This is my main page</div>
+    <div>
+        <span>This is my main page</span>
+        <div>
+            <span>This is a button:</span>
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        </div>
+    </div>
 )
