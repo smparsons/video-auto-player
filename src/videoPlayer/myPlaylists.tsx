8c391409fa3fe3d2027cd8d98fcb7d5f73d9c399
Samuel Parsons <samuel.m.parsons@gmail.com>
2019-08-24 16:21:43 -0400

    Read json from file

--- src/videoPlayer/myPlaylists.tsx
+++ src/videoPlayer/myPlaylists.tsx
@@ -3,9 +3,24 @@ import * as React from 'react'
 
 import { ImportFileButton } from '../components'
 
-const processFile = (fileList: FileList | null): void => {
+const readPlaylistJsonFromFile = (file: File): Promise<Playlist> => new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onload = () => {
+        const fileReadResult = reader.result as string
+        const playlistJson = JSON.parse(fileReadResult)
+        resolve(playlistJson)
+    }
+    reader.readAsText(file)
+})
+
+const processFile = async (fileList: FileList | null): Promise<void> => {
+    const importedPlaylist = fileList && fileList[0]
+    if (!importedPlaylist) {
+        throw new Error("There was a problem trying to read the imported file.");
+    }
+    const playlistJson = await readPlaylistJsonFromFile(importedPlaylist)
     // tslint:disable-next-line
-    console.log(JSON.stringify(fileList))
+    console.log(JSON.stringify(playlistJson))
 }
 
 export const MyPlaylists = (): JSX.Element => {
@@ -22,4 +37,20 @@ export const MyPlaylists = (): JSX.Element => {
             </ImportFileButton>
         </>
     )
+}
+
+interface Playlist {
+    groups: VideoGroup[]
+}
+
+interface VideoGroup {
+    groupId: number
+    groupName: string
+    videos: Video[]
+}
+
+interface Video {
+    description: string
+    title: string
+    url: string
 }
