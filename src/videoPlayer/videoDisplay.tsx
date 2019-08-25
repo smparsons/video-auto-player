4087b3b6bce00cb55d09ec7dc36dd0b259c4a963
Samuel Parsons <samuel.m.parsons@gmail.com>
2019-08-25 17:30:03 -0400

    Autoplay video

--- src/videoPlayer/videoDisplay.tsx
+++ src/videoPlayer/videoDisplay.tsx
@@ -4,17 +4,31 @@ import * as React from 'react'
 import { Video } from './playlistTypes'
 
 export const VideoDisplay = ({ video }: VideoDisplayProps) => {
+    const videoRef = React.useRef(null) as React.RefObject<HTMLVideoElement>
+
+    React.useEffect(() => {
+        const videoDomObject = videoRef.current
+        if (videoDomObject) {
+            videoDomObject.play()
+        }
+    })
+
     return (
         <Paper className="video-player-section">
             <div className="video-display">
                 {video
                     ? (
                         <>
-                            <Typography variant="h5">
-                                {video.title}
-                            </Typography>
+                            <div>
+                                <Typography variant="h5">
+                                    {video.title}
+                                </Typography>
+                                <Typography>
+                                    {video.subtitle}
+                                </Typography>
+                            </div>
                             <div key={video.url}>
-                                <video controls={true}>
+                                <video controls={true} ref={videoRef}>
                                     <source src={video.url} />
                                 </video>
                             </div>
