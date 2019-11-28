93dd7615e637d445b49d8a58bc41f5c47cfa0753
Samuel Parsons <samuel.m.parsons@gmail.com>
2019-11-28 13:07:54 -0500

    Adding temporary console log

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { PlaylistBuilder } from './playlistBuilder'
 import { VideoPlayerPage } from './videoPlayer'
 
+console.log(`My public url is: ${process.env.PUBLIC_URL}`);
+
 export const AppRouter = (): JSX.Element => (
     <Router basename={`${process.env.PUBLIC_URL}/`}>
         <Switch>
